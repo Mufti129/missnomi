8e99daa1d8e64a28e9fce795b465361e0203cbd3
Mufti129 <ukhtytrader01@gmail.com>
2026-02-20 15:34:13 +0700

    dash_miss.py

--- dash_miss.py
+++ dash_miss.py
@@ -171,7 +171,7 @@ if prod_multi:
 
 st.subheader("Preview Data Setelah Filter")
 #st.dataframe(df_filtered.head(300))
-with st.expander("Metodologi & Rule of Thumb", expanded=False):
+with st.expander("Dataset", expanded=False):
         st.dataframe(df_filtered.head(300)) 
         
 
@@ -1426,6 +1426,7 @@ else:
     if apply_log:
         st.warning("Transform log1p diterapkan pada data — hasil forecast dalam skala log1p. Untuk interpretasi, gunakan inverse np.expm1.")
     st.info("by Mukhammad Rekza Mufti-Data Analis")
+
 
 
 
